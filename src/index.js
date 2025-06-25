f460a147e6b1f2f84384d4b631ee0b04a2e1db14
Anirudh Kumbhar <anirudhkumbhar5@gmail.com>
2025-06-25 10:21:16 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongo db error",err)
+})
